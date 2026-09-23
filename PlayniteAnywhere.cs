b69c0dfc2244a86c410ac82adf9320a7cb734087
Yann <y.faure@groupe-esigelec.fr>
2026-09-23 22:42:40 +0200

    Toolbox new GenericPlugin

---
+++ PlayniteAnywhere.cs
@@ -0,0 +1,82 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Events;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PlayniteAnywhere
+{
+    public class PlayniteAnywhere : GenericPlugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private PlayniteAnywhereSettingsViewModel settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("1b4b762f-1a9a-423b-9643-1f323ef5ef71");
+
+        public PlayniteAnywhere(IPlayniteAPI api) : base(api)
+        {
+            settings = new PlayniteAnywhereSettingsViewModel(this);
+            Properties = new GenericPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public override void OnGameInstalled(OnGameInstalledEventArgs args)
+        {
+            // Add code to be executed when game is finished installing.
+        }
+
+        public override void OnGameStarted(OnGameStartedEventArgs args)
+        {
+            // Add code to be executed when game is started running.
+        }
+
+        public override void OnGameStarting(OnGameStartingEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameStopped(OnGameStoppedEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameUninstalled(OnGameUninstalledEventArgs args)
+        {
+            // Add code to be executed when game is uninstalled.
+        }
+
+        public override void OnApplicationStarted(OnApplicationStartedEventArgs args)
+        {
+            // Add code to be executed when Playnite is initialized.
+            logger.Info("Playnite Anywhere started.");
+        }
+
+        public override void OnApplicationStopped(OnApplicationStoppedEventArgs args)
+        {
+            // Add code to be executed when Playnite is shutting down.
+        }
+
+        public override void OnLibraryUpdated(OnLibraryUpdatedEventArgs args)
+        {
+            // Add code to be executed when library is updated.
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new PlayniteAnywhereSettingsView();
+        }
+    }
+}
